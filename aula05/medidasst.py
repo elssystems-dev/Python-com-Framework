1733c5344640161dbc908f793499fb407d3c7e0f
edxzyluksz <edxzyluksz@gmail.com>
2025-11-04 21:51:01 -0300

    Fix

--- aula05/medidasst.py
+++ aula05/medidasst.py
@@ -2,8 +2,9 @@ import streamlit as st
 import math as mt
 
 # Problemas medidas 
-TITULO = "Cálcilo de área de um quadrado, triângulo e trapézio"
-st.title(TITULO)
+TITULO = "Cálculo de área de um quadrado, triângulo e trapézio"
+st.set_page_config(page_title=f"{TITULO}")
+st.markdown(f"<h1 style='text-align: center; margin-bottom: 10vh;'>{TITULO}</h1>", unsafe_allow_html=True)
 
 # Entrada de dados
 medidaA = st.number_input("Inserir medida (Base):")
@@ -16,6 +17,7 @@ areaTriangulo = (medidaA * medidaB) / 2
 areaTrapezio = ((medidaA + medidaC) * medidaB) / 2
 
 # Saída de dados
-st.write(f"A área do quadrado é: {areaQuadrado:.4f}")
-st.write(f"A área do triângulo é: {areaTriangulo:.4f}")
-st.write(f"A área do trapézio é: {areaTrapezio:.4f}")
+st.markdown("<h2 style='text-align: left;'>Resultados:</h2>", unsafe_allow_html=True)
+st.write(f"A área do quadrado é: {areaQuadrado:.4f} m²")
+st.write(f"A área do triângulo é: {areaTriangulo:.4f} m²")
+st.write(f"A área do trapézio é: {areaTrapezio:.4f} m²")
