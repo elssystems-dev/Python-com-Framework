ba0cc13e368157c297ba350c3a3a651155881ed3
edxzyluksz <edxzyluksz@gmail.com>
2025-11-05 21:15:47 -0300

    aula06

--- aula06/bhaskaraST.py
+++ aula06/bhaskaraST.py
@@ -1,36 +1,35 @@
-import streamlit as st
-import math as mt
+from streamlit import header, write, text_input, button, warning, success, error
+from math import sqrt, pow
 
 delta = "(esperando dados...)"
 
-st.header('Calculadora de Bháskara')
-st.write("alculadora de raízes \n de uma equação de segundo grau")
-st.write("ax² + bx + c = 0")
+header('Calculadora de Bháskara')
+write("alculadora de raízes \n de uma equação de segundo grau")
+write("ax² + bx + c = 0")
 
 
 # Entrada de dados
-a = st.text_input('Digite o valor de a:')
-b = st.text_input('Digite o valor de b:')
-c = st.text_input('Digite o valor de c:')
+a = text_input('Digite o valor de a:')
+b = text_input('Digite o valor de b:')
+c = text_input('Digite o valor de c:')
 
 # Processamento de dados
-if st.button("Calcular raízes"):
+if button("Calcular raízes"):
     try:
         a = float(a)
         b= float(b)
         c = float(c)
-        delta = mt.pow(b, 2) - 4 * a * c
+        delta = pow(b, 2) - 4 * a * c
         if delta < 0:
-            st.warning("A equação não possui raízes reais")
+            warning("A equação não possui raízes reais")
         elif delta == 0:
-            raiz = round((-b + mt.sqrt(delta)) / (2*a), ndigits=3)
-            st.success(f"A equação possui uma raíz real: {raiz}")
+            raiz = round((-b + sqrt(delta)) / (2*a), ndigits=3)
+            success(f"A equação possui uma raíz real: {raiz}")
         else:
-            raiz1 = round((-b + mt.sqrt(delta)) / (2*a), ndigits=3)
-            raiz2 = round((-b - mt.sqrt(delta)) / (2*a), ndigits=3)
-            st.success(f"As raízes da equação são:\n Raiz 1: {raiz1} \n Raiz 2: {raiz2} ")
+            raiz1 = round((-b + sqrt(delta)) / (2*a), ndigits=3)
+            raiz2 = round((-b - sqrt(delta)) / (2*a), ndigits=3)
+            success(f"As raízes da equação são:\n Raiz 1: {raiz1} \n Raiz 2: {raiz2} ")
     except:
-        st.error("Por favor, insira valores válidos para a, b e c.")
+        error("Por favor, insira valores válidos para a, b e c.")
 
-st.write("O valor de delta é ", delta)
-
+write("O valor de delta é ", delta)
