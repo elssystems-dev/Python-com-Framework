c3e701e899904b6e33dd06231eae5f257cacaf57
Eduardo Lucas Garcia <dev.eduardols@gmail.com>
2025-11-06 19:47:15 -0300

    aula07 v2

--- aula07/dardoST.py
+++ aula07/dardoST.py
@@ -13,15 +13,18 @@ with coluna2:
     dardo2 = st.number_input("Distância do 2º dardo", min_value=0.0)
 with coluna3:
     dardo3 = st.number_input("Distância do 3º dardo", min_value=0.0)
+maior_distancia = max(dardo1, dardo2, dardo3)
 
 # Estrutura de controle de decisão
 if (dardo1 > dardo2) and (dardo1 > dardo3):
     dardo_vencedor = "Dardo 1"
 elif (dardo2 > dardo1) and (dardo2 > dardo3):
     dardo_vencedor = "Dardo 2"
+elif (dardo1 == dardo2) and (dardo1 == dardo3):
+    dardo_vencedor = "Empate"
 else:
     dardo_vencedor = "Dardo 3"
 
 # Saída de dados
 if st.button("Apresentar dados de lançamento"):
-    st.success(f"O dardo com a maior distância é {dardo_vencedor}")
+    st.success(f"O dardo com a maior distância é {dardo_vencedor} com {maior_distancia}")
