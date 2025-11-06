db8dabb610aafe9de95969a3857a6a8b4a5bb96f
edxzyluksz <edxzyluksz@gmail.com>
2025-11-06 20:46:30 -0300

    aula07 v3

--- aula07/dardoST.py
+++ aula07/dardoST.py
@@ -1,4 +1,20 @@
 import streamlit as st
+
+def grafico(dardo1, dardo2, dardo3):
+    # Apresentação de gráfico exibindo lançamento
+    st.area_chart([0, dardo1], 
+                use_container_width=True,
+                height=200,
+                color="#eaff00")
+    st.area_chart([0, dardo2], 
+                use_container_width=True,
+                height=200,
+                color="#ffa600")
+    st.area_chart([0, dardo3], 
+                use_container_width=True,
+                height=200,
+                color="#235fc7")
+
 st.title("🎯 Simulação de lançamento de dardos 🎯")
 
 '''Simulação de lançamento de três dardos. O objetivo do aplicativo é 
@@ -16,15 +32,24 @@ with coluna3:
 maior_distancia = max(dardo1, dardo2, dardo3)
 
 # Estrutura de controle de decisão
-if (dardo1 > dardo2) and (dardo1 > dardo3):
-    dardo_vencedor = "Dardo 1"
-elif (dardo2 > dardo1) and (dardo2 > dardo3):
-    dardo_vencedor = "Dardo 2"
-elif (dardo1 == dardo2) and (dardo1 == dardo3):
-    dardo_vencedor = "Empate"
+if (dardo1 == maior_distancia) and (dardo1 != dardo2) and (dardo1 != dardo3):
+    dardo_vencedor = "1º dardo"
+elif (dardo2 == maior_distancia) and (dardo2 != dardo1) and (dardo2 != dardo3):
+    dardo_vencedor = "2º dardo"
+elif (dardo3 == maior_distancia) and (dardo3 != dardo1) and (dardo3 != dardo2):
+    dardo_vencedor = "3º dardo"
 else:
-    dardo_vencedor = "Dardo 3"
+    dardo_vencedor = "Empate"
 
 # Saída de dados
 if st.button("Apresentar dados de lançamento"):
-    st.success(f"O dardo com a maior distância é {dardo_vencedor} com {maior_distancia}")
+    if dardo_vencedor == "Empate":
+        st.warning("Houve um empate entre os dardos lançados.")
+        st.write(f"As distâncias máximas foram: {maior_distancia}")
+    else:
+        st.success(f"O dardo com a maior distância é {dardo_vencedor} com {maior_distancia}")
+        grafico(dardo1, dardo2, dardo3)
+        st.balloons()
+
+
+# Copilot, não sei se voc
