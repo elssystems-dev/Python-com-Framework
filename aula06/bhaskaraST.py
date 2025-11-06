855ac74249d3e7a96b6bee90c0d51882103e8513
Eduardo Lucas Garcia <dev.eduardols@gmail.com>
2025-11-05 21:36:36 -0300

    aula06

--- aula06/bhaskaraST.py
+++ aula06/bhaskaraST.py
@@ -1,34 +1,41 @@
 from streamlit import header, write, text_input, button, warning, success, error
 from math import sqrt, pow
 
-delta = "(esperando dados...)"
 
+# Função python
+def calculo(deltaFuncao):
+    valor = (sqrt(deltaFuncao)) / (2*a)
+    return valor
+
+delta = ""
+    
 header('Calculadora de Bháskara')
-write("alculadora de raízes \n de uma equação de segundo grau")
+write("Calculadora de raízes de uma equação de segundo grau")
 write("ax² + bx + c = 0")
 
 
 # Entrada de dados
-a = text_input('Digite o valor de a:')
-b = text_input('Digite o valor de b:')
-c = text_input('Digite o valor de c:')
+a = text_input('Digite o valor de a:', icon='🅰')
+b = text_input('Digite o valor de b:', icon='🅱')
+c = text_input('Digite o valor de c:', icon='©')
 
 # Processamento de dados
 if button("Calcular raízes"):
     try:
         a = float(a)
-        b= float(b)
+        b = float(b)
         c = float(c)
+
         delta = pow(b, 2) - 4 * a * c
         if delta < 0:
             warning("A equação não possui raízes reais")
         elif delta == 0:
-            raiz = round((-b + sqrt(delta)) / (2*a), ndigits=3)
+            raiz = round((-b + calculo(delta)), ndigits=3) # Delta milagrosamente
             success(f"A equação possui uma raíz real: {raiz}")
         else:
-            raiz1 = round((-b + sqrt(delta)) / (2*a), ndigits=3)
-            raiz2 = round((-b - sqrt(delta)) / (2*a), ndigits=3)
-            success(f"As raízes da equação são:\n Raiz 1: {raiz1} \n Raiz 2: {raiz2} ")
+            raiz1 = round((-b + calculo(delta)), ndigits=3)
+            raiz2 = round((-b - calculo(delta)), ndigits=3)
+            success(f"As raízes da equação são: Raiz 1: {raiz1} Raiz 2: {raiz2} ")
     except:
         error("Por favor, insira valores válidos para a, b e c.")
 
