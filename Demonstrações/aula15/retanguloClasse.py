33a8f54d91ad29f928d717d9cf6f1a3aa67f85ab
edxzyluksz <edxzyluksz@gmail.com>
2025-11-18 21:43:46 -0300

    aula15

---
+++ Demonstrações/aula15/retanguloClasse.py
@@ -0,0 +1,59 @@
+# ----------- Início Classe -----------
+
+class Retangulo:
+    # Membros da classe
+
+    # 1º Membro - Atributos
+    __base:float
+    __altura:float
+
+    # 2º Membro - Propriedades
+    # Propriedade da base
+    @property # Pegar o valor do atributo
+    def base(self):
+        return self.__base
+    @base.setter # Definir o valor do atributo
+    def base(self, base:float):
+        if base < 0:
+            base = 0
+        self.__base = base
+    @property # Pegar o valor do atributo
+    def altura(self):
+        return self.__altura
+    @altura.setter # Definir o valor do atributo
+    def altura(self, altura:float):
+        if altura < 0:
+            altura = 0
+        self.__altura = altura
+
+    #3º Membro - Construtor
+    def __init__(self, base:float, altura:float):
+        self.base = base
+        self.altura = altura
+    
+    #4º Membro - Métodos
+    def area(self) -> float:
+        return self.altura * self.base
+
+    def perimetro(self) -> float:
+        return(self.base * 2) + (self.altura * 2)
+    
+    def diagonal(self) -> float:
+        from math import sqrt, pow
+        return sqrt(pow(self.base, 2) + pow(self.altura, 2))
+    
+    def dadosRetangulo(self) -> str:
+        saida = f'''Área = {self.area()}\nPerímetro = {self.perimetro()}\nDiagonal = {self.diagonal()} '''
+        return saida
+    
+# ----------- Fim classe ----------- 
+
+# ----------- Início programa principal ----------- 
+
+# Entrada de dados
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+# Instanciar um objeto do tipo retângulo
+retangulo = Retangulo(base, altura)
+# Saída de dados
+print(retangulo.dadosRetangulo())
